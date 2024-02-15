fab298591c37a19a6c298b1103715dc0b99b7ca2
Giovani Freitag <giovani.freitag@plantae.agr.br>
2024-02-15 00:01:11 -0400

    Find and Filter functions

---
+++ index.js
@@ -0,0 +1,29 @@
+(function($) {
+
+    const originalFind = $.fn.find;
+    const originalFilter = $.fn.filter;
+
+    $.fn.extend({
+        find: function(selector, cb = null){
+    
+            const ctx = originalFind.call(this, selector);
+    
+            if(!cb)
+                return ctx;
+    
+            cb.call(ctx, ctx);
+            return this;
+        },
+        filter: function(selector, cb = null){
+    
+            const el = originalFilter.call(this, selector);
+    
+            if(!cb)
+                return ctx;
+    
+            cb.call(ctx, ctx);
+            return this;
+        }
+    });
+
+})(jQuery);
